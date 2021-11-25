f3db77d53d238bc5bbcce1ec8bc52980d806fd7e
sungmuk Kang <miranaky@gmail.com>
2021-11-25 22:09:02 +0900

    FEAT: Create get user's tire infomations.
    - '/api/v1/users/<str:id>/' 사용자 아이디를 통해서 저장된 타이어 정보를 조회할 수 있다.
    - 로그인 된 사용자(jwt token 을 갖은 사용자)만 정보를 조회할 수 있다.
    - 로그인 되어 있지 않은 사용자는 status code 401을 받는다.
    - 사용자 아이디가 없다면 status code 404를 받는다.

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from users.views import UserCreateView, login
+from users.views import UserCreateView, UserTiresInfoView, login
 
 app_name = "users"
 
 urlpatterns = [
     path("", UserCreateView.as_view()),
     path("login/", login),
+    path("<str:username>/", UserTiresInfoView.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,16 @@
 import jwt
 from django.conf import settings
 from django.contrib.auth import authenticate
+from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, RetrieveAPIView
 from rest_framework.mixins import CreateModelMixin
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
+from tires.models import TiresInfo
+from tires.serializers import TireInfoReadSerializer
 from users.models import User
 from users.serializers import UserSerializer
 
@@ -26,7 +29,6 @@ class UserCreateView(CreateModelMixin, GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user = self.perform_create(serializer)
-            print(user.pk)
             headers = self.get_success_headers(serializer.data)
             encoded_jwt = jwt.encode(
                 {"pk": user.pk}, settings.SECRET_KEY, algorithm="HS256"
@@ -61,3 +63,39 @@ def login(request):
         return Response(data={"access_token": encoded_jwt})
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserTiresInfoView(RetrieveAPIView):
+    queryset = TiresInfo.objects.all()
+    serializer_class = TireInfoReadSerializer
+    permission_classes = [IsAuthenticated]
+    lookup_url_kwarg = "username"
+    lookup_field = "username"
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        page = self.paginate_queryset(instance)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(instance, many=True)
+        return Response(serializer.data)
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+        assert lookup_url_kwarg in self.kwargs, (
+            "Expected view %s to be called with a URL keyword argument "
+            'named "%s". Fix your URL conf, or set the `.lookup_field` '
+            "attribute on the view correctly."
+            % (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        user = get_object_or_404(User, **filter_kwargs)
+        obj = get_list_or_404(queryset, username=user.id)
+
+        self.check_object_permissions(self.request, obj)
+        return obj
