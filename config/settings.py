ec6322e5a666f5c18ffa6ccd1e3ca23523a7401c
sungmuk Kang <miranaky@gmail.com>
2021-11-27 01:48:50 +0900

    CHORE: add deploy setting

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
+ALLOWED_HOSTS = ["web", "localhost", "127.0.0.1", "testserver"]
 
 
 # Application definition
